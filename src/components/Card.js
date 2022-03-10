63646f9f416bd31728e60803b9f9406b068868ef
Toskan4134 <Neonxservers@gmail.com>
2022-03-10 01:06:13 +0100

    Cards añadidos pero falta color de las tags

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,53 @@
-import React,{useRef} from 'react';//Todo
+import React, { useEffect } from 'react';
 import './Card.css';
 import { Link } from 'react-router-dom';
 
-function Card({ path, label, image, text, width, isUrl, tagColor  }) {
-    width = width ? width : "auto";
-    const tag = useRef();
-    if(tagColor){
-        var r = document.querySelector('tag');
-            tag.current.style.setProperty('--background-color', tagColor);
+function Card({ path, label, image, text, width, isUrl, tagColor }) {
+    width = width ? width : 'auto';
+    useEffect(() => {
+        var cardTag = document.getElementById('card-tag');
+        if (tagColor) {
+            cardTag.style.setProperty('--background-color', tagColor);
+        }
+    }, []);
+
+    if (isUrl) {
+        return (
+            <li className='card-container' style={{ width: width }}>
+                <a className='cards__item__link' href={path}>
+                    <div
+                        id='card-tag'
+                        className='cards__item__pic-wrap'
+                        style={{ backgroundColor: '#fff' }}
+                        data-category={label}
+                    >
+                        <img className='cards__item__img' src={image} />
+                    </div>
+                    <div className='cards__item__info'>
+                        <h5 className='cards__item__text'>{text}</h5>
+                    </div>
+                </a>
+            </li>
+        );
+    } else {
+        return (
+            <li className='card-container' style={{ width: width }}>
+                <Link className='cards__item__link' to={path}>
+                    <div
+                        id='card-tag'
+                        className='cards__item__pic-wrap'
+                        style={{ backgroundColor: '#fff' }}
+                        data-category={label}
+                    >
+                        <img className='cards__item__img' src={image} />
+                    </div>
+                    <div className='cards__item__info'>
+                        <h5 className='cards__item__text'>{text}</h5>
+                    </div>
+                </Link>
+            </li>
+        );
     }
-    return (
-        <li className="card-container" style={{ maxWidth: width }}>
-            <Link className='cards__item__link' to={path}>
-                <figure ref={tag} className='cards__item__pic-wrap' style={{backgroundColor: '#fff'}} data-category={label}>
-                    <img
-                        className='cards__item__img'
-                        src={image}
-                    />
-                </figure>
-                <div className='cards__item__info'>
-                    <h5 className='cards__item__text'>{text}</h5>
-                </div>
-            </Link>
-        </li>
-    );
 }
 
 export default Card;
--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../App.css';
 import Footer from '../Footer.js';
 import Card from '../Card.js';
+import './Inicio.css';
 import { Heading } from '../Heading';
 
 function Inicio() {
@@ -17,8 +18,36 @@ function Inicio() {
                 btnTxt2='Botón 2'
                 buttons='2'
             />
-            <div className="container">
-                <Card tagColor="red" width="200px" label="Importante" path="https://google.com" text="Hola" image="https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg"  />
+            <div className='container'>
+                <div className='cards-container'>
+                    <Card
+                        isUrl
+                        tagColor='#ff2828'
+                        width='30%'
+                        label='New'
+                        path='https://neonx.page.link/discord'
+                        text='Nuevo servidor de discord de NeonX (GTA V)'
+                        image='https://i.blogs.es/760434/discordportada/1366_2000.jpeg'
+                    />
+                    <Card
+                        isUrl
+                        tagColor='blue'
+                        width='30%'
+                        label='Hot'
+                        path='https://google.com'
+                        text='Nuevo servidor de discord de NeonX (GTA V)'
+                        image='https://larepublica.pe/resizer/xr757tkwMqyNRTMX1tdsiOQ2pZU=/1200x660/top/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/3SXXEFW7IVG5JF4H7WSA3ZYEVE.png'
+                    />
+                    <Card
+                        isUrl
+                        tagColor='#ff2828'
+                        width='30%'
+                        label='New'
+                        path='https://google.com'
+                        text='Nuevo servidor de discord de NeonX (GTA V)'
+                        image='https://larepublica.pe/resizer/xr757tkwMqyNRTMX1tdsiOQ2pZU=/1200x660/top/cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/3SXXEFW7IVG5JF4H7WSA3ZYEVE.png'
+                    />
+                </div>
             </div>
             <Footer />
         </>
