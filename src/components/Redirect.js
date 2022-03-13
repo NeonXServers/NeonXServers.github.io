4905ab8e1d8a9997b34903b958ed001d1d86fffe
Toskan4134 <Neonxservers@gmail.com>
2022-03-13 14:04:14 +0100

    Updates

--- src/components/Redirect.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-let Redirect;
-export default Redirect = ({ url }) => {
-    useEffect(() => {
-        window.location.href = '' + url;
-    }, []);
-    return null;
-};
